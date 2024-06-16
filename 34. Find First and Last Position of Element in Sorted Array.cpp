c8ed1839bd4fe1f6a8e7ea741f462528b4b3b469
Utkarsh Pratap Singh <singhup0911@gmail.com>
2024-06-16 17:44:29 +0530

    New

--- 34. Find First and Last Position of Element in Sorted Array.cpp
+++ 34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1,m;
-        while(l<=h){
-            m=(l+h)/2;
-            if(nums[m] == target){
-                l=h=m;
-                while(l>0){
-                    if(nums[l-1] != target)
-                        break;
-                    else
-                        l--;
-                }
-                while(h<nums.size()-1){
-                    if(nums[h+1] != target)
-                        break;
-                    else
-                        h++;
-                }
-                return {l,h};
-            }
-            else if(nums[m] < target){
-                l=m+1;
-            }
-            else{
-                h=m-1;
-            }
+        int n = nums.size();
+
+        if(n == 0){
+            return {-1,-1};
         }
+
+        int low = 0;
+        int high = n -1;
+
+        while(low<=high){
+
+            int mid = (high - low)/2 + low;
+
+            if(nums[mid] == target){
+
+                low = mid;
+                high = mid;
+
+                while(low>0){
+                    if(nums[low-1] != target){
+                        break;
+                    }
+                    else{
+                        low--;
+                    }
+                }
+                while(high<n-1){
+                    if(nums[high+1] != target){
+                        break;
+                    }
+                    else{
+                        high++;
+                    }
+                }
+                return {low,high};
+
+            }
+
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+
+            else{
+                high = mid - 1;
+            }
+
+        }
+
         return {-1,-1};
     }
 };
