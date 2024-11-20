214b7d055b1e92fb5c4a0ad79c9b392bf759deca
Utkarsh Pratap Singh <singhup0911@gmail.com>
2024-11-20 23:28:34 +0530

    Problems

--- 155. Min Stack.cpp
+++ 155. Min Stack.cpp
@@ -28,11 +28,35 @@ public:
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+// OR (better approach i feel)
+
+class MinStack {
+public:
+
+    stack<pair<int, int> > s;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int ele;
+        if(s.empty()){
+            ele = val;
+        }else{
+            ele = min(s.top().second, val);
+        }
+        s.push(make_pair(val,ele));
+    }
+    
+    void pop() {
+        s.pop();
+    }
+    
+    int top() {
+        return s.top().first;
+    }
+    
+    int getMin() {
+        return s.top().second;
+    }
+};
